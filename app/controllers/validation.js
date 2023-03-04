93889bac24333fa985ea129f35cd76720af91757
Duong Thanh Tam <thanhtamduong555@gmail.com>
2023-03-05 01:42:27 +0700

    first commit

---
+++ app/controllers/validation.js
@@ -0,0 +1,37 @@
+function Validation() {
+  this.kiemTraRong = function (value, spanId, mess) {
+    if (value === "") {
+      getEle(spanId).style.display = "block";
+      getEle(spanId).innerHTML = mess;
+      return false;
+    }
+    getEle(spanId).style.display = "none";
+    getEle(spanId).innerHTML = "";
+    return true;
+  };
+  this.kiemTraChuoiKyTu = function (value, spanId, mess) {
+    var letter =
+      "^[a-zA-Z_ÀÁÂÃÈÉÊẾÌÍÒÓÔÕÙÚĂĐĨŨƠàáâãèéêìíòóôõùúăđĩũơƯĂẠẢẤẦẨẪẬẮẰẲẴẶ" +
+      "ẸẺẼỀỀỂưăạảấầẩẫậắằẳẵặẹẻẽềềểếỄỆỈỊỌỎỐỒỔỖỘỚỜỞỠỢỤỦỨỪễệỉịọỏốồổỗộớờởỡợ" +
+      "ụủứừỬỮỰỲỴÝỶỸửữựỳỵỷỹ\\s]+$";
+    if (value.match(letter)) {
+      getEle(spanId).style.display = "none";
+      getEle(spanId).innerHTML = "";
+      return true;
+    }
+    getEle(spanId).style.display = "block";
+    getEle(spanId).innerHTML = mess;
+    return false;
+  };
+  this.kiemTraSo = function (value, spanId, mess) {
+    var letter = "/^[-+]?[0-9]+$/";
+    if (value.match(letter)) {
+      getEle(spanId).style.display = "none";
+      getEle(spanId).innerHTML = "";
+      return true;
+    }
+    getEle(spanId).style.display = "block";
+    getEle(spanId).innerHTML = mess;
+    return false;
+  };
+}
